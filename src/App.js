07bcfb7198431be823ff054384e03d33102a78cc
Neamoth <Neamothcse@gmail.com>
2022-01-28 15:47:12 +0600

    feat: Text field for convert text

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import Navbar from './components/Navbar';
+import TextFrom from './components/TextFrom';
 import './App.css';
 function App() {
   return (
     <>
-    <Navbar/>
+    <Navbar logoTitle="Conveter"/>
+    <div className="container">
+      <TextFrom heading="Enter your text here"/>
+    </div>
     </>
   );
 }
